38cd138934aa8ebeea2fb2d5eee8117b6b66413c
Augustus Bailey <gusbailey@Augustuss-MacBook-Pro.local>
2021-11-17 11:05:38 -0800

    removed main and added canvas var"

--- src/org/headroyce/dp1/Graphics.java
+++ src/org/headroyce/dp1/Graphics.java
@@ -1,4 +1,12 @@
 package org.headroyce.dp1;
 
+import javafx.scene.canvas.Canvas;
+
 public class Graphics {
+    private Canvas canvas;
+
+    public Graphics() {
+        canvas.setHeight(100);
+        canvas.setWidth(200);
+    }
 }
