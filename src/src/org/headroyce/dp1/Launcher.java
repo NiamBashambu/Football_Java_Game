0ead2988517a9820733f8c80f8e221e6df2a2e78
gusbailey <augustusb2023@headroyce.org>
2021-11-18 10:42:11 -0800

    launcher stuff

--- src/src/org/headroyce/dp1/Launcher.java
+++ src/src/org/headroyce/dp1/Launcher.java
@@ -1,11 +1,16 @@
 package org.headroyce.dp1;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
@@ -15,9 +20,25 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 300, 250));
+        DrawingWorkspace dw = new DrawingWorkspace();
+        primaryStage.setTitle("Canvas");
+        primaryStage.setScene(new Scene(dw, 800, 600, Color.GREEN));
         primaryStage.show();
+
+        primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                dw.refreshScreen();
+            }
+        });
+
+        primaryStage.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                dw.refreshScreen();
+            }
+        });
+
+        dw.refreshScreen();
     }
 }
