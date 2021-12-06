245a8725933887372c1c3932802dcd523ef847dc
niamb2023 <niamb2023@headroyce.org>
2021-12-06 13:24:39 -0800

    fixing most of the stuff except fixing the sprite showing up on the screen

--- src/src/org/headroyce/dp1/Sprite.java
+++ src/src/org/headroyce/dp1/Sprite.java
@@ -7,42 +7,25 @@ import javafx.scene.shape.Circle;
 
 public abstract class Sprite {
 
-    /**
-     * Current display node
-     */
+
     public Node node;
 
-    /**
-     * velocity vector x direction
-     */
+
     public double vX = 0;
 
-    /**
-     * velocity vector y direction
-     */
+
     public double vY = 0;
 
-    /**
-     * dead?
-     */
-    public boolean isDead = false;
 
-    /**
-     * collision shape
-     */
+
+
+
     public Circle collisionBounds;
 
-    /**
-     * Updates this sprite object's velocity, or animations.
-     */
+
     public abstract void update();
 
-    /**
-     * Did this sprite collide into the other sprite?
-     *
-     * @param other - The other sprite.
-     * @return boolean - Whether this or the other sprite collided, otherwise false.
-     */
+
     public boolean collide(Sprite other) {
 
         if (collisionBounds == null || other.collisionBounds == null) {
