a6fb847dd24c7609be2abddd74a388bf7c069b43
gusbailey <augustusb2023@headroyce.org>
2022-05-09 13:33:25 -0700

    stuff

--- src/src/org/headroyce/dp1/Receiver.java
+++ src/src/org/headroyce/dp1/Receiver.java
@@ -1,7 +1,7 @@
 package org.headroyce.dp1;
 
 //another version of the sprite class, also the main version
-public class Receiver extends Sprite{
+public class Receiver extends Sprite {
     public Receiver(double x , double y, double vX, double vY, DrawingWorkspace dw){
         super(x, y, vX, vY, 20, 20, "WR", x, y, dw);
     }
